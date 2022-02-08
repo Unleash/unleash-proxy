3f3300be8a25603ee8823e5ed5d56595360cda8a
Ivar Conradi Østhus <ivarconr@gmail.com>
2022-02-08 11:26:25 +0100

    fix: reset example

--- example.js
+++ example.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 const { createApp } = require('./dist/app');
 
 const app = createApp({
-    unleashUrl: 'https://app2.unleash-hosted.com/demo/api/',
+    unleashUrl: 'https://app.unleash-hosted.com/demo/api/',
     unleashApiToken: '56907a2fa53c1d16101d509a10b78e36190b0f918d9f122d',
     clientKeys: ['proxy-secret', 'another-proxy-secret', 's1'],
     refreshInterval: 1000,
