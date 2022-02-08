e7365e620118371cdd30df7f63d2bfb77ca601be
Ivar Conradi Østhus <ivarconr@gmail.com>
2022-02-08 17:51:49 +0100

    fix: add start on metrics

--- src/client.ts
+++ src/client.ts
@@ -96,6 +96,7 @@ class Client extends EventEmitter implements IClient {
         this.unleash.on('ready', () => {
             this.emit('ready');
             this.ready = true;
+            this.metrics.start();
         });
     }
 
