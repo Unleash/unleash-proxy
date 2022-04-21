bc3edad17b35a9fe346721db08a120d5c1125e52
Thomas Heartman <thomas@getunleash.ai>
2022-04-21 12:27:26 +0200

    feat: add openapi types from TS

---
+++ src/Controller.ts
@@ -0,0 +1,71 @@
+import {
+    Request as ExpressRequest,
+    Response as ExpressResponse,
+} from 'express';
+import { Controller, Get, Request, Response, Route } from 'tsoa';
+
+type Variant = {
+    payload?: string;
+    name: string;
+    enabled: boolean;
+};
+
+type FeatureResponse = {
+    name: string;
+    enabled: boolean;
+    variant: Variant;
+};
+
+/**
+ This is a model description.
+ It describes the features response model.
+*/
+type FeaturesResponse = {
+    /**
+   The list of features that are enabled with the given context.
+  */
+    toggles: FeatureResponse[];
+};
+
+type CustomValidationError = {
+    message: string;
+    context: any;
+};
+
+@Route('/proxy2')
+export class MainController extends Controller {
+    /**
+     * A very long, verbose, wordy, long-winded, tedious, verbacious, tautological,
+     * profuse, expansive, enthusiastic, redundant, flowery, eloquent, articulate,
+     * loquacious, garrulous, chatty, extended, babbling description.
+     * @summary A concise summary.
+     */
+    @Get('')
+    @Response<CustomValidationError>(
+        503,
+        'The Unleash Proxy  is not ready to accept requests yet.',
+    )
+    @Response(
+        401,
+        'Unauthorized; the client key you provided is not valid for this instance.',
+    )
+    public async getToggles(
+        @Request() req: ExpressRequest,
+    ): Promise<FeaturesResponse | string | void> {
+        const apiToken = req.header('authorization');
+
+        if (false) {
+            this.setStatus(503);
+            return 'Not ready';
+            // } else if (!apiToken || !['mykey'].includes(apiToken)) {
+            //     this.setStatus(401);
+        } else {
+            const { query } = req;
+            query.remoteAddress = query.remoteAddress || req.ip;
+            // const context = createContext(query);
+            // const toggles = this.client.getEnabledToggles(context);
+            this.setHeader('Cache-control', 'public, max-age=2');
+            return { toggles: [] };
+        }
+    }
+}
--- src/app.ts
+++ src/app.ts
@@ -1,52 +1,62 @@
 import compression from 'compression';
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import Client, { IClient } from './client';
 import { createProxyConfig, IProxyOption } from './config';
 
 import UnleashProxy from './unleash-proxy';
 import { OpenApiService } from './openapi/openapi-service';
+import { RegisterRoutes } from './routes';
+import { generateHTML, serve } from 'swagger-ui-express';
 
 const corsOptions = {
-    exposedHeaders: 'ETag',
-    maxAge: 172800,
+  exposedHeaders: 'ETag',
+  maxAge: 172800,
 };
 
 export function createApp(
-    options: IProxyOption,
-    unleashClient?: IClient,
-    app: Application = express(),
+  options: IProxyOption,
+  unleashClient?: IClient,
+  app: Application = express(),
 ): Application {
-    const config = createProxyConfig(options);
-    const client = unleashClient || new Client(config);
+  const config = createProxyConfig(options);
+  const client = unleashClient || new Client(config);
 
-    const openApiService = new OpenApiService(config);
+  const openApiService = new OpenApiService(config);
 
-    const proxy = new UnleashProxy(client, config, openApiService);
+  const proxy = new UnleashProxy(client, config, openApiService);
 
-    openApiService.useDocs(app);
+  openApiService.useDocs(app);
 
-    app.disable('x-powered-by');
-    try {
-        app.set('trust proxy', config.trustProxy);
-    } catch (err) {
-        config.logger.error(
-            `The provided "trustProxy" option was not valid ("${config.trustProxy}")`,
-            err,
-        );
-    }
-
-    app.use(cors(corsOptions));
-
-    app.use(compression());
-
-    app.use(
-        `${config.proxyBasePath}/proxy`,
-        cors(corsOptions),
-        express.json(),
-        proxy.middleware,
+  app.disable('x-powered-by');
+  try {
+    app.set('trust proxy', config.trustProxy);
+  } catch (err) {
+    config.logger.error(
+      `The provided "trustProxy" option was not valid ("${config.trustProxy}")`,
+      err,
     );
-    return app;
+  }
+
+  app.use(cors(corsOptions));
+
+  app.use(compression());
+
+  app.use(
+    `${config.proxyBasePath}/proxy`,
+    cors(corsOptions),
+    express.json(),
+    proxy.middleware,
+  );
+
+  app.use("/docs", serve, async (_: Request, res: Response) => {
+    return res.send(
+      generateHTML(await import ("./swagger.json"))
+    )
+  })
+  RegisterRoutes(app);
+
+  return app;
 }
 
 module.exports = { createApp };
