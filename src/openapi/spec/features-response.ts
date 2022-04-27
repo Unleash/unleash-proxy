91354f9dddb4fcd15e797d0daf7f0b09056270d1
Thomas Heartman <thomas@getunleash.ai>
2022-04-27 12:24:53 +0200

    docs: properly describe features endpoint.

--- src/openapi/spec/features-response.ts
+++ src/openapi/spec/features-response.ts
@@ -2,7 +2,7 @@ import { OpenAPIV3 } from 'openapi-types';
 import { CreateSchemaType } from '../openapi-types';
 
 export const featuresResponse: OpenAPIV3.ResponseObject = {
-    description: 'featuresResponse',
+    description: 'The list of enabled toggles for the provided context.',
     content: {
         'application/json': {
             schema: {
