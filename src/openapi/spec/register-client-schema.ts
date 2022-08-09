4ccc5e950fc5c302fb3d2f430f3441cc5030d912
Ivar Conradi Østhus <ivarconr@gmail.com>
2022-08-09 13:16:49 +0200

    fix: add descriptons to register client schema

--- src/openapi/spec/register-client-schema.ts
+++ src/openapi/spec/register-client-schema.ts
@@ -6,30 +6,41 @@ export const schema = {
     properties: {
         appName: {
             type: 'string',
+            description: 'Name of the application using Unleash',
         },
         instanceId: {
             type: 'string',
+            description:
+                'Instance id for this application (typically hostname, podId or similar)',
         },
         sdkVersion: {
             type: 'string',
+            description:
+                'Optional field that describes the sdk version (name:version)',
         },
         environment: {
             type: 'string',
+            deprecated: true,
         },
         interval: {
             type: 'number',
+            description:
+                'At which interval, in milliseconds, will this client be expected to send metrics',
         },
         started: {
             oneOf: [
                 { type: 'string', format: 'date-time' },
                 { type: 'number' },
             ],
+            description:
+                'When this client started. Should be reported as ISO8601 time.',
         },
         strategies: {
             type: 'array',
             items: {
                 type: 'string',
             },
+            description: 'List of strategies implemented by this application',
         },
     },
 } as const;
