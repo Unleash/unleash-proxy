3201ab739e845a235110149c5724b493bbbe1b8b
Ivar Conradi Østhus <ivar@getunleash.io>
2023-01-24 19:55:39 +0100

    feat: add support for metrics_jitter (#113)

--- src/config.ts
+++ src/config.ts
@@ -23,6 +23,7 @@ export interface IProxyOption {
     proxyBasePath?: string;
     refreshInterval?: number;
     metricsInterval?: number;
+    metricsJitter?: number;
     environment?: string;
     projectName?: string;
     logger?: Logger;
@@ -53,6 +54,7 @@ export interface IProxyConfig {
     proxyBasePath: string;
     refreshInterval: number;
     metricsInterval: number;
+    metricsJitter: number;
     environment?: string;
     projectName?: string;
     logger: Logger;
@@ -328,6 +330,9 @@ export function createProxyConfig(option: IProxyOption): IProxyConfig {
         metricsInterval:
             option.metricsInterval ||
             safeNumber(process.env.UNLEASH_METRICS_INTERVAL, 30_000),
+        metricsJitter:
+            option.metricsJitter ||
+            safeNumber(process.env.UNLEASH_METRICS_JITTER, 0),
         environment: option.environment || process.env.UNLEASH_ENVIRONMENT,
         projectName: option.projectName || process.env.UNLEASH_PROJECT_NAME,
         namePrefix: option.namePrefix || process.env.UNLEASH_NAME_PREFIX,
