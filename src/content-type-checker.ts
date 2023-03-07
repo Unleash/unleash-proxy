e35b866a0b1b15ced7f96a011820e5403657d1d2
Thomas Heartman <thomas@getunleash.ai>
2023-03-07 12:28:28 +0100

    Docs: update description of the content-type-checker middleware

--- src/content-type-checker.ts
+++ src/content-type-checker.ts
@@ -4,10 +4,21 @@ import { hasBody, is } from 'type-is';
 const DEFAULT_ACCEPTED_CONTENT_TYPE = 'application/json';
 
 /**
- * Builds an express middleware checking the content-type header
- * returning 415 if the header is not either `application/json` or in the array
- * passed into the function of valid content-types
- * @param {String} acceptedContentTypes
+ * Builds an express middleware that checks the content-type header of requests with bodies.
+ *
+ * If the request has no body (as determined by the `type-is` library's
+ * `hasBody` function), this middleware passes it along to the next in line.
+ *
+ * If the request has a body, it checks whether the content-type matches one of
+ * the accepted content types, returning a 415 if it doesn't.
+ *
+ * If the request has a body, but not no content-type header, it will modify the
+ * request and add the first accepted content type as the content-type header,
+ * passing it along to the next request handler.
+ *
+ * @param {String} acceptedContentTypes - The list of content-types the middleware
+ * should accept. Defaults to ['application/json'] if none are provided.
+ *
  * @returns {function(Request, Response, NextFunction): void}
  */
 export default function requireContentType(
