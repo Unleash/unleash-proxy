Unleash/unleash-proxy

commit 547524c0ca139ddbad62ee499eb38ae33e3c6a9c

fix: Add content-type support (#119)
**tl;dr:**
- Add content-type middleware
- Reject (415) requests with wrong content-type
- Assume that if there is no content-type, it should be JSON

These changes enable content-type validation for the proxy by using a piece of [express middleware from Unleash](https://github.com/Unleash/unleash/blob/dab55bf3c6ecc66049b5e645ef968fa76991f22f/src/lib/middleware/content_type_checker.ts). The goal is to ensure that requests made with the wrong content-type are told that we don't accept that content-type (415 response). 

It also adds 415 responses to the OpenAPI description for the relevant endpoints.

## Changes from Unleash's content-type checker

The content-type checker has been modified slightly from Unleash's content-type checker:
1. If the request has no body, the middleware is a no-op. This means we can apply the middleware to all operations, and it's not going to add content-types to GET requests or DELETE requests without bodies, for instance. It's also not going to add anything to POST requests without bodies.
2. If the request has a body, but no content-type, the middleware assumes you forgot to add it (**be liberal in what you accept**) and will add the first accepted content-type. This might not work in all cases, but should cover most use cases where that happens.

As with the other version of the middleware, requests with wrong content-types are rejected.

## Files

Most of the changes should be small enough to inspect easily, but there's two files that are new. They have been adapted and taken from Unleash. Here's a description of what I've done to them:

### `src/content-type-checker.ts`

This is adapted from the [Unleash-version of the middleware](https://github.com/Unleash/unleash/blob/dab55bf3c6ecc66049b5e645ef968fa76991f22f/src/lib/middleware/content_type_checker.ts). Some slight changes to the logic and to the description.

### `src/test/content-type-checker.test.ts`

src/app.ts
@@ -6,6 +6,7 @@ import { createProxyConfig, IProxyOption } from './config';
 
 import UnleashProxy from './unleash-proxy';
 import { OpenApiService } from './openapi/openapi-service';
+import requireContentType from './content-type-checker';
 
 export function createApp(
     options: IProxyOption,
@@ -43,6 +44,8 @@ export function createApp(
     app.use(cors(corsOptions));
 
     app.use(compression());
+
+    app.use(requireContentType());
 
     app.use(
         `${config.proxyBasePath}/proxy`,

src/content-type-checker.ts
@@ -1,0 +1,46 @@
+import { RequestHandler } from 'express';
+import { hasBody, is } from 'type-is';
+
+const DEFAULT_ACCEPTED_CONTENT_TYPE = 'application/json';
+
+/**
+ * Builds an express middleware that checks the content-type header of requests with bodies.
+ *
+ * If the request has no body (as determined by the `type-is` library's
+ * `hasBody` function), this middleware passes it along to the next in line.
+ *
+ * If the request has a body, it checks whether the content-type matches one of
+ * the accepted content types, returning a 415 if it doesn't.
+ *
+ * If the request has a body, but not no content-type header, it will modify the
+ * request and add the first accepted content type as the content-type header,
+ * passing it along to the next request handler.
+ *
+ * @param {String} acceptedContentTypes - The list of content-types the middleware
+ * should accept. Defaults to ['application/json'] if none are provided.
+ *
+ * @returns {function(Request, Response, NextFunction): void}
+ */
+export default function requireContentType(
+    ...acceptedContentTypes: string[]
+): RequestHandler {
+    if (acceptedContentTypes.length === 0) {
+        acceptedContentTypes.push(DEFAULT_ACCEPTED_CONTENT_TYPE);
+    }
+    return (req, res, next) => {
+        const contentType = req.header('Content-Type');
+
+        if (hasBody(req)) {
+            if (!contentType) {
+                req.headers['content-type'] = acceptedContentTypes[0];
+                next();
+            } else if (is(contentType, acceptedContentTypes)) {
+                next();
+            } else {
+                res.status(415).end();
+            }
+        } else {
+            next();
+        }
+    };
+}

src/openapi/common-responses.ts
@@ -68,6 +68,19 @@ export const emptySuccessResponse: OpenAPIV3.ResponseObject = {
     },
 };
 
+export const unsupportedMediaTypeResponse: OpenAPIV3.ResponseObject = {
+    description:
+        'The operation does not support request payloads of the provided type.',
+    content: {
+        'text/plain': {
+            schema: {
+                type: 'string',
+                example: 'Unsupported media type',
+            },
+        },
+    },
+};
+
 export const internalServerErrorResponse: OpenAPIV3.ResponseObject = {
     description:
         "Something went wrong on the server side and we were unable to recover. If you have custom strategies loaded, make sure they don't throw errors.",
@@ -116,6 +129,7 @@ const commonResponses = {
     200: emptySuccessResponse,
     400: badRequestResponse,
     401: unauthorizedResponse,
+    415: unsupportedMediaTypeResponse,
     500: internalServerErrorResponse,
     501: notImplementedError,
     503: notReadyResponse,

src/test/__snapshots__/openapi.test.ts.snap
@@ -735,6 +735,17 @@ If you don't provide the \`toggles\` property, then this operation functions exa
           "401": Object {
             "description": "Authorization information is missing or invalid.",
           },
+          "415": Object {
+            "content": Object {
+              "text/plain": Object {
+                "schema": Object {
+                  "example": "Unsupported media type",
+                  "type": "string",
+                },
+              },
+            },
+            "description": "The operation does not support request payloads of the provided type.",
+          },
           "500": Object {
             "content": Object {
               "application/json": Object {
@@ -931,6 +942,17 @@ If you don't provide the \`toggles\` property, then this operation functions exa
           },
           "401": Object {
             "description": "Authorization information is missing or invalid.",
+          },
+          "415": Object {
+            "content": Object {
+              "text/plain": Object {
+                "schema": Object {
+                  "example": "Unsupported media type",
+                  "type": "string",
+                },
+              },
+            },
+            "description": "The operation does not support request payloads of the provided type.",
           },
           "500": Object {
             "content": Object {

src/test/content-type-checker.test.ts
@@ -1,0 +1,194 @@
+import { Request, Response } from 'express';
+import requireContentType from '../content-type-checker';
+
+const mockRequest =
+    (transferEncoding: string | undefined) =>
+    (contentType?: string): Request => {
+        const headers = {
+            'content-type': contentType,
+            'transfer-encoding': transferEncoding,
+        };
+
+        // @ts-expect-error (It's not a real request)
+        return {
+            header: (name: string) =>
+                //@ts-expect-error (yes, this may fail)
+                headers[name.toLowerCase()],
+            headers,
+        };
+    };
+
+const mockRequestWithBody = mockRequest('chunked');
+
+const mockRequestWithoutBody = mockRequest(undefined);
+
+const returns415: (t: jest.Mock) => Response = (t) => ({
+    // @ts-ignore
+    status: (code) => {
+        expect(415).toBe(code);
+        return {
+            end: t,
+        };
+    },
+});
+
+const expectNoCall: (t: jest.Mock) => Response = (t) => ({
+    // @ts-ignore
+    status: () => ({
+        // @ts-ignore
+        end: () => expect(t).toHaveBeenCalledTimes(0),
+    }),
+});
+
+describe('Content-type checker middleware', () => {
+    test('should by default only support application/json', () => {
+        const middleware = requireContentType();
+        const t = jest.fn();
+        const fail = jest.fn();
+        middleware(
+            mockRequestWithBody('application/json'),
+            expectNoCall(fail),
+            t,
+        );
+        middleware(mockRequestWithBody('text/plain'), returns415(t), fail);
+        expect(t).toHaveBeenCalledTimes(2);
+        expect(fail).toHaveBeenCalledTimes(0);
+    });
+
+    test('should support application/json with charset by default', () => {
+        const middleware = requireContentType();
+        const t = jest.fn();
+        const fail = jest.fn();
+        middleware(
+            mockRequestWithBody('application/json; charset=UTF-8'),
+            expectNoCall(fail),
+            t,
+        );
+        middleware(mockRequestWithBody('text/plain'), returns415(t), fail);
+        expect(t).toHaveBeenCalledTimes(2);
+        expect(fail).toHaveBeenCalledTimes(0);
+    });
+
+    test('should allow adding custom supported types', () => {
+        const middleware = requireContentType('application/yaml');
+        const t = jest.fn();
+        const fail = jest.fn();
+        middleware(
+            mockRequestWithBody('application/yaml'),
+            expectNoCall(fail),
+            t,
+        );
+        middleware(mockRequestWithBody('text/html'), returns415(t), fail);
+        middleware(mockRequestWithBody('text/plain'), returns415(t), fail);
+        expect(t).toHaveBeenCalledTimes(3);
+        expect(fail).toHaveBeenCalledTimes(0);
+    });
+
+    test('does not support default support types if you provide your own', () => {
+        const middleware = requireContentType('application/yaml');
+        const t = jest.fn();
+        const fail = jest.fn();
+        middleware(
+            mockRequestWithBody('application/json'),
+            returns415(t),
+            fail,
+        );
+        expect(t).toHaveBeenCalledTimes(1);
+        expect(fail).toHaveBeenCalledTimes(0);
+    });
+
+    test('Should accept multiple content-types', () => {
+        const middleware = requireContentType(
+            'application/json',
+            'application/yaml',
+            'form/multipart',
+        );
+        const fail = jest.fn();
+        const succeed = jest.fn();
+        middleware(
+            mockRequestWithBody('application/json'),
+            expectNoCall(fail),
+            succeed,
+        );
+        middleware(
+            mockRequestWithBody('application/yaml'),
+            expectNoCall(fail),
+            succeed,
+        );
+        middleware(
+            mockRequestWithBody('form/multipart'),
+            expectNoCall(fail),
+            succeed,
+        );
+        middleware(
+            mockRequestWithBody('text/plain'),
+            returns415(succeed),
+            fail,
+        );
+        expect(succeed).toHaveBeenCalledTimes(4);
+        expect(fail).toHaveBeenCalledTimes(0);
+    });
+
+    test('should not stop requests that have no body', () => {
+        const middleware = requireContentType('application/yaml');
+        const t = jest.fn();
+        const fail = jest.fn();
+        middleware(
+            mockRequestWithBody('application/json'),
+            returns415(t),
+            fail,
+        );
+        expect(t).toHaveBeenCalledTimes(1);
+        expect(fail).toHaveBeenCalledTimes(0);
+    });
+
+    describe('add default content-types to requests with bodies but no content-type', () => {
+        test('should add the default content type if no custom types are provided', () => {
+            const middleware = requireContentType();
+            const request = mockRequestWithBody();
+            const t = jest.fn();
+            const fail = jest.fn();
+            middleware(request, expectNoCall(fail), t);
+            expect(t).toHaveBeenCalledTimes(1);
+            expect(request.header('content-type')).toEqual('application/json');
+        });
+
+        test('does not add default content type if there is no body', () => {
+            const middleware = requireContentType();
+            const request = mockRequestWithoutBody();
+            const t = jest.fn();
+            const fail = jest.fn();
+            middleware(request, expectNoCall(fail), t);
+            expect(t).toHaveBeenCalledTimes(1);
+            expect(request.header('content-type')).toEqual(undefined);
+        });
+
+        test('should add the first custom content type if provided', () => {
+            const middleware = requireContentType(
+                'application/yaml',
+                'application/xml',
+                'application/x-www-form-urlencoded',
+            );
+
+            const request = mockRequestWithBody();
+            const t = jest.fn();
+            const fail = jest.fn();
+            middleware(request, expectNoCall(fail), t);
+            expect(t).toHaveBeenCalledTimes(1);
+            expect(request.header('content-type')).toEqual('application/yaml');
+        });
+
+        test('does not change the content-type if the request already has one', () => {
+            const middleware = requireContentType(
+                'application/json',
+                'application/yaml',
+            );
+            const request = mockRequestWithBody('application/yaml');
+            const t = jest.fn();
+            const fail = jest.fn();
+            middleware(request, expectNoCall(fail), t);
+            expect(t).toHaveBeenCalledTimes(1);
+            expect(request.header('content-type')).toEqual('application/yaml');
+        });
+    });
+});

src/test/unleash-proxy.test.ts
@@ -684,3 +684,65 @@ test('Should return 501 when all feature toggles is not enabled', () => {
         .set('Authorization', 'sdf')
         .expect(501);
 });
+
+describe('Request content-types', () => {
+    test.each(['/proxy', '/proxy/all'])(
+        'Should assign default content-type if the request has a body but no content-type (%s)',
+        async (endpoint) => {
+            const client = new MockClient();
+            const payload = {
+                context: { appName: 'my-app' },
+            };
+
+            const proxySecrets = ['sdf'];
+            const app = createApp(
+                {
+                    unleashUrl,
+                    unleashApiToken,
+                    proxySecrets,
+                    enableAllEndpoint: true,
+                },
+                client,
+            );
+            client.emit('ready');
+
+            await request(app)
+                .post(endpoint)
+                .set('Authorization', 'sdf')
+                .set('Content-Type', '')
+                .send(payload)
+                .expect(200)
+                .then(() => {
+                    expect(client.queriedContexts[0].appName).toEqual(
+                        payload.context.appName,
+                    );
+                });
+        },
+    );
+
+    test.each(['/proxy', '/proxy/all'])(
+        'Should reject non-"content-type: application/json" for POST requests to %s',
+        (endpoint) => {
+            const client = new MockClient();
+
+            const proxySecrets = ['sdf'];
+            const app = createApp(
+                {
+                    unleashUrl,
+                    unleashApiToken,
+                    proxySecrets,
+                    enableAllEndpoint: true,
+                },
+                client,
+            );
+            client.emit('ready');
+
+            return request(app)
+                .post(endpoint)
+                .set('Authorization', 'sdf')
+                .set('Content-Type', 'application/html')
+                .send('<em>reject me!</em>')
+                .expect(415);
+        },
+    );
+});

src/unleash-proxy.ts
@@ -143,7 +143,7 @@ However, using this endpoint will increase the payload size transmitted to your 
             openApiService.validPath({
                 requestBody: lookupTogglesRequest,
                 responses: {
-                    ...standardResponses(401, 500, 501, 503),
+                    ...standardResponses(401, 415, 500, 501, 503),
                     200: featuresResponse,
                 },
                 description: `This endpoint accepts a JSON object with a \`context\` property and an optional \`toggles\` property.
@@ -163,7 +163,7 @@ If you don't provide the \`toggles\` property, then this operation functions exa
             openApiService.validPath({
                 requestBody: lookupTogglesRequest,
                 responses: {
-                    ...standardResponses(400, 401, 500, 503),
+                    ...standardResponses(400, 401, 415, 500, 503),
                     200: featuresResponse,
                 },
                 description: `This endpoint accepts a JSON object with a \`context\` property and an optional \`toggles\` property.

commit 0d03235b56ba15bb628b05b3ea3e89ab7943b297

fix: move required content type check (#126)
> Solves problem introduced in https://github.com/Unleash/unleash-proxy/pull/119
> If application in general supports other content types (file upload for example) then now it doesn't. Moving required types checking to proxy endpoints resolves problem/limitation

When this was merged previously, it was done in such a way that it would apply to the entire app if one was passed in. When moved into its new positions, it only applies to the proxy endpoints.

src/app.ts
@@ -45,10 +45,9 @@ export function createApp(
 
     app.use(compression());
 
-    app.use(requireContentType());
-
     app.use(
         `${config.proxyBasePath}/proxy`,
+        requireContentType(),
         cors(corsOptions),
         express.json(),
         proxy.middleware,