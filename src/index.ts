df5579baed5f7500bb712cbff5afc313df14dc38
Christopher Kolstad <chriswk@getunleash.io>
2024-10-01 09:34:18 +0200

    chore: bump to upstream version of openapi

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-/// <reference path="./types/openapi.d.ts" />
-
-import { createApp } from './app';
-import Client from './client';
-import { createProxyConfig } from './config';
-import { start } from './server';
+import { createApp } from "./app";
+import Client from "./client";
+import { createProxyConfig } from "./config";
+import { start } from "./server";
 
 export { createApp, start, Client, createProxyConfig };
--- src/types/openapi.d.ts
+++ src/types/openapi.d.ts
@@ -1,16 +1,16 @@
 // Partial types for "@unleash/express-openapi".
-declare module '@unleash/express-openapi' {
-    import type { RequestHandler } from 'express';
+declare module "@wesleytodd/openapi" {
+  import type { RequestHandler } from "express";
 
-    export interface IExpressOpenApi extends RequestHandler {
-        validPath: (operation: OpenAPIV3.OperationObject) => RequestHandler;
-        schema: (name: string, schema: OpenAPIV3.SchemaObject) => void;
-        swaggerui: RequestHandler;
-    }
+  export interface IExpressOpenApi extends RequestHandler {
+    validPath: (operation: OpenAPIV3.OperationObject) => RequestHandler;
+    schema: (name: string, schema: OpenAPIV3.SchemaObject) => void;
+    swaggerui: () => RequestHandler;
+  }
 
-    export default function openapi(
-        docsPath: string,
-        document: Omit<OpenAPIV3.Document, 'paths'>,
-        options?: { coerce: boolean },
-    ): IExpressOpenApi;
+  export default function openapi(
+    docsPath: string,
+    document: Omit<OpenAPIV3.Document, "paths">,
+    options?: { coerce: boolean },
+  ): IExpressOpenApi;
 }
